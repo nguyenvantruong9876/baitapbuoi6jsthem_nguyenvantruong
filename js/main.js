df457fb69c83830595014848a85ea5cafd2ba006
Truong <nguyenvantruong9876>
2022-09-17 15:42:43 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,33 +1,30 @@
 // bài 3 Tính Giai thừa
 function kiemtra(num) {
-    var kq = true;
-    
     if (num < 2) {
-        kq = false;
-    } else if (num == 2) {
-        kq = true;
-    }else if (num % 2 == 0) {
-        kq = false;
-    }else {
-        for (var i = 3; i <=  Math.sqrt(num); i += 2) {
-            if (num % i == 0) {
-                return  false;
-                
-            }
+        return false;
+      }
+      for (var i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i == 0) {
+          return false;
         }
-    }
-    return kq;
+      }
+      return true;
 }
 function inso() {
     var n = document.getElementById("nhapinso").value;
     var inra = '';
-
-    for (var i = 1; i <= n; i++) {
-        if (kiemtra(i)) {
-            inra += "  " + i;
+    if (n < 2) {
+        alert("Bạn hãy nhập số lớn hơn 2");
+    } 
+    else{
+        for (var i = 1; i <= n; i++) {
+            if (kiemtra(i) == true) {
+                inra += i + " ";
+            }
+            document.getElementById("txtResultinso").innerHTML = inra;
         }
-        document.getElementById("txtResultinso").innerHTML = inra;
     }
+    
 
 }
 document.getElementById("btninso").onclick = inso;
